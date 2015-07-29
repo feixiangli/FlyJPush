9571d830be5f65ef2ea35c3189268e6ed60c915d
Fly.李飞翔 <1985228464@qq.com>
2015-07-29 18:28:33 +0800

    Initial Commit

---
+++ FlyJPush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlyJPush
+//
+//  Created by 李飞翔 on 15/7/29.
+//  Copyright (c) 2015年 Fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlyJPush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlyJPush
+//
+//  Created by 李飞翔 on 15/7/29.
+//  Copyright (c) 2015年 Fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
